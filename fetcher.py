dd48f41057ef39f50796dab5002390f4cbcfe28b
MacKaiver <mackaiver@freenet.de>
2014-02-27 15:58:12 +0100

    Made postitional arguments optional

--- fetcher.py
+++ fetcher.py
@@ -3,13 +3,28 @@ __author__ = 'mackaiver'
 #Lets start with a simple commandline tool
 
 import argparse
+import urllib.request
+
+
+class Fetcher:
+    url = 'http://mobile.bahn.de/bin/mobil/query2.exe/dox'
+
+    def get_efa(self, departure, arrival):
+        response = urllib.request.urlopen(self.url)
+        data = response.read()
+        print(data)
+
 
 if __name__ == '__main__':
-    p = argparse.ArgumentParser(description='Lecker data fetching from EFA via the commandline. ')
+    p = argparse.ArgumentParser(description='Lecker data fetching from EFA via the commandline. ',
+                                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    p.add_argument('from', default='Universität s-Bahnhof, Dortmund', metavar='from', type=str,
+    p.add_argument('-d', default='Universität s-Bahnhof, Dortmund', metavar='from', type=str,
                    help='Name of the departing station')
-    p.add_argument('to', default='Dortmund hbf', metavar='to', type=str, help='Name of the arrival station')
+    p.add_argument('-a', default='Dortmund hbf', metavar='to', type=str, help='Name of the arrival station')
 
     args = p.parse_args()
-    print(args)
+    f = Fetcher()
+    f.get_efa(args.d, args.a)
+
+
