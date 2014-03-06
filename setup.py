796750d91be7772fc959c4bb50ae686ba94b6f96
MacKaiver <mackaiver@freenet.de>
2014-03-06 11:12:23 +0100

    A setup.py which provides two entry points to the cmd_line tool

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
-from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
+
 setup(
     name = 'seins',
-    packages = ['seins'], # this must be the same as the name above
+    packages=find_packages(),
     version = '0.3',
-    description = 'A small command line utility to show you when the next train to your desired location is arriving',
+    description='A  small module and  command line utility to show you when the '
+                'next train to your desired location is arriving by parsing the DB website',
     author = 'Kai',
     author_email = 'kai@woistbier.de',
     install_requires = [ 'colorama', 'requests', ' beautifulsoup4' ],
     url = 'https://github.com/mackaiver/sEins-Server',   # use the URL to the github repo
-    download_url = 'https://github.com/mackaiver/sEins-Server/archive/master.zip', # I'll explain this in a second
-    keywords = ['traffic', 'scrapping', 'utility', 'transportation', 's1', 'DB'], # arbitrary keywords
+    download_url='https://github.com/mackaiver/sEins-Server/archive/master.zip',
+    keywords=['traffic', 'scrapping', 'utility', 'transportation', 's1', 'DB'],
     license = 'MIT',
     classifiers = [
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
     ],
     entry_points={
-        'console_scripts':
-            ['seins = seins.seins:main',
-             ]}
+        "console_scripts": [
+            "s1 = cmd_line.seins_cmd:main",
+            "seins = cmd_line.seins_cmd:main",
+        ],
+    }
 )
